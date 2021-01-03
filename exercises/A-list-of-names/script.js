caf0eea4bc4b0b68c912523b9386e72073d204d2
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2021-01-03 16:20:14 +0000

    refactored exercise 1

---
+++ exercises/A-list-of-names/script.js
@@ -0,0 +1,11 @@
+function exerciseOne(arrayOfPeople) {
+  let content = document.querySelector("#content");
+}
+
+let people = [
+  { name: "Chris", job: "Teacher" },
+  { name: "Joanna", job: "Student" },
+  { name: "Boris", job: "Prime Minister" }
+];
+  
+exerciseOne(people);
